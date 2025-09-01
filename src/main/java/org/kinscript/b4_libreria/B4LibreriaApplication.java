df453da3c3f308b9375270fdc315f3750f00681b
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2025-09-01 11:11:09 -0600

    init: Initial Commit

---
+++ src/main/java/org/kinscript/b4_libreria/B4LibreriaApplication.java
@@ -0,0 +1,13 @@
+package org.kinscript.b4_libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B4LibreriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B4LibreriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kinscript/b4_libreria/B4LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kinscript.b4_libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B4LibreriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
